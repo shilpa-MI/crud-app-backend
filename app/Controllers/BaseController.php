bf5008ae3ac2648a8bf98e22e6474310402bb771
Shilpa Jesadiya <shilpa.jesadiya@mindinventory.com>
2021-03-01 14:53:38 +0530

    - Fixed issue with parameters

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -96,7 +96,6 @@ class BaseController extends Controller
     public function getRequestInput(IncomingRequest $request){
         $input = $request->getPost();
         if (empty($input)) {
-            //convert request body to associative array
             $input = json_decode($request->getBody(), true);
         }
         return $input;
@@ -111,17 +110,12 @@ class BaseController extends Controller
      */
     public function validateRequest($input, array $rules, array $messages =[]){
         $this->validator = Services::Validation()->setRules($rules);
-        // If you replace the $rules array with the name of the group
         if (is_string($rules)) {
             $validation = config('Validation');
 
-            // If the rule wasn't found in the \Config\Validation, we
-            // should throw an exception so the developer can find it.
             if (!isset($validation->$rules)) {
                 throw ValidationException::forRuleNotFound($rules);
             }
-
-            // If no error message is defined, use the error message in the Config\Validation file
             if (!$messages) {
                 $errorName = $rules . '_errors';
                 $messages = $validation->$errorName ?? [];
